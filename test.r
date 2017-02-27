ff6d017e9369a500476d6ee143d4c640abc3987e
lambertrocher <lambert.rocher@gmail.com>
2017-02-27 16:44:55 +0100

    Add files via upload

--- test.r
+++ test.r
@@ -6,7 +6,7 @@ r <- GET("http://httpbin.org/get");
 print(r);
 
 GET("http://localhost:8086/query", 
-  query = list(key1 = "CREATE DATABASE mydb")
+  query = list(q = "CREATE DATABASE mydb")
 )
 
-POST("http://localhost:8086/write?db=mydb", body = list("cpu_load_short","host=server01","region=us-west"))
+POST("http://localhost:8086/write?db=mydb", body = "cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000")
